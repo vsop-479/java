ad447c9965a63e2f290f6ca831596963dc7093ee
zhouhui <zhouhui@360.net>
2018-08-13 11:57:31 +0800

    cat sort

--- core/src/main/java/algorithm/catSort/Test.java
+++ core/src/main/java/algorithm/catSort/Test.java
@@ -2,25 +2,31 @@ package algorithm.catSort;
 
 import algorithm.catSort.model.Cat;
 import algorithm.catSort.model.Color;
-
 import java.util.Random;
 
+/**
+ * 将数组中的白猫排在黑猫前面。
+ */
 public class Test {
     static Random random = new Random(77);
-    public static Cat[] cats = new Cat[10];
 
-    public static void generateCats(){
+    public static Cat[] generateCats(int length){
+        Cat[] cats = new Cat[length];
         for(int i = 0; i < cats.length; i++){
-            if(random.nextInt() / 2 == 0){
-                cats[i] = new Cat(Color.BLACK);
+            if(random.nextInt() % 2 == 0){
+                cats[i] = new Cat(Color.BLACK, i + "");
             }else{
-                cats[i] = new Cat(Color.WHITE);
+                cats[i] = new Cat(Color.WHITE, i + "");
             }
         }
+        return cats;
     }
 
     public static void main(String[] args){
-
+        Cat[] cats = generateCats(10);
+        printCats(cats);
+        sort(cats);
+        printCats(cats);
     }
 
     public static void sort(Cat[] cats){
@@ -35,10 +41,20 @@ public class Test {
                         head++;
                         tail--;
                 }else{
-
+                        head++;
                 }
+            }else{
+                tail--;
             }
         }
+    }
+
+    public static void printCats(Cat[] cats){
+        for(Cat cat : cats){
+            System.out.print(cat);
+            System.out.print(",");
+        }
+        System.out.println();
     }
 
 }
--- core/src/main/java/algorithm/catSort/model/Cat.java
+++ core/src/main/java/algorithm/catSort/model/Cat.java
@@ -2,9 +2,11 @@ package algorithm.catSort.model;
 
 public class Cat {
     private Color color;
+    private String name;
 
-    public Cat (Color color){
+    public Cat (Color color, String name){
         this.color = color;
+        this.name = name;
     }
 
     public Color getColor() {
@@ -13,5 +15,20 @@ public class Cat {
 
     public void setColor(Color color) {
         this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                color + name +
+                '}';
     }
 }
