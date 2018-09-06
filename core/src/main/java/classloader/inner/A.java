246d5a5ac0fa956f1c79e3e9928bdf3198ab6cac
zhouhui <zhouhui@360.net>
2018-09-06 11:52:32 +0800

    内部类是否会被初始化测试。

--- core/src/main/java/classloader/inner/A.java
+++ core/src/main/java/classloader/inner/A.java
@@ -1,14 +1,20 @@
 package classloader.inner;
 
+
 public class A {
     static{
         System.out.println("A");
+    }
+
+    public static A getInstance(){
+        return InnerA.a;
     }
 
     private static class InnerA{
         static{
             System.out.println("InnerA");
         }
+        private static A a = new A();
     }
 
 }
