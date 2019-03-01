6d28a81d382d54866c76f81583bd804faf17788d
zhouhui <zhouhui@360.net>
2019-03-01 17:59:37 +0800

    PrintClassLoader.

--- core/src/main/java/classloader/showLoaders/PrintClassLoader.java
+++ core/src/main/java/classloader/showLoaders/PrintClassLoader.java
@@ -1,11 +1,14 @@
 package classloader.showLoaders;
 
+import com.sun.javafx.util.Logging;
+
 import java.util.HashMap;
 
 public class PrintClassLoader {
     public static void main(String[] args)
     {
         System.out.println("ClassLoader of PrintClassLoader: " + PrintClassLoader.class.getClassLoader());
+        System.out.println("ClassLoader of Logging: " + Logging.class.getClassLoader());
         System.out.println("ClassLoader of HashMap: " + HashMap.class.getClassLoader());
 
         ClassLoader classLoader = PrintClassLoader.class.getClassLoader();
