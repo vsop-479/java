91ff4e354d61d84d5513b2a6060159babdfa3d29
zhouhui <zhouhui@360.net>
2019-10-17 15:07:16 +0800

    test unsafe.

---
+++ core/src/main/java/unsafe/Test.java
@@ -0,0 +1,32 @@
+package unsafe;
+
+import sun.misc.Unsafe;
+import java.lang.reflect.Field;
+
+public class Test {
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+    }
+
+    /**
+     *  直接get会有权限问题
+     *  java.lang.SecurityException: Unsafe
+     * @return
+     */
+    public static Unsafe getSecurityException(){
+        Unsafe u = Unsafe.getUnsafe();
+        return u;
+    }
+
+    /**
+     *  适用反射获取field
+      * @return
+     * @throws NoSuchFieldException
+     * @throws IllegalAccessException
+     */
+    public static Unsafe getReflect() throws NoSuchFieldException, IllegalAccessException {
+        Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
+        theUnsafe.setAccessible(true);
+        Unsafe unsafe = (Unsafe) theUnsafe.get(null);
+        return unsafe;
+    }
+}
