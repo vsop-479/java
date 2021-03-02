f4da723cccc2c30cc20a7f62cf6b424e6e4cf827
vsop-479 <vsop_479@163.com>
2021-03-02 10:30:59 +0800

    numMatrix

---
+++ core/src/main/java/algorithm/leetcode/numMatrix/NumMatrix.java
@@ -0,0 +1,40 @@
+package algorithm.leetcode.numMatrix;
+
+/**
+ * https://leetcode-cn.com/problems/range-sum-query-2d-immutable/
+ *
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */
+
+public class NumMatrix {
+    private int[][] matrix;
+
+    public NumMatrix(int[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int sum = 0;
+        for(int i = row1; i <= row2; i++){
+            for(int j = col1; j <= col2; j++){
+                sum += matrix[i][j];
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{
+                {3, 0, 1, 4, 2},
+                {5, 6, 3, 2, 1},
+                {1, 2, 0, 1, 5},
+                {4, 1, 0, 1, 7},
+                {1, 0, 3, 0, 5}};
+        NumMatrix numMatrix = new NumMatrix(matrix);
+        System.out.println(numMatrix.sumRegion(2, 1, 4, 3));
+        System.out.println(numMatrix.sumRegion(1, 1, 2, 2));
+        System.out.println(numMatrix.sumRegion(1, 2, 2, 4));
+    }
+}
